a271f9d66055d378def7a02de7cc31f59e3a7fc1
Brian-Enos <brianenos9199@gmail.com>
2023-06-13 10:38:28 +0300

    Populated the content of the main section of the project

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -6,8 +6,15 @@ import Styles from "../CSS/styles.css";
 
 export default function Main() {
   return (
-    <div>
-      <h1>Main Component Goes here</h1>
-    </div>
+    <main>
+      <h1 className="main--title">Fun Facts About React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke </li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powre thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
   )
 }
