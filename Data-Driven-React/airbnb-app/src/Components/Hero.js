bcf838d3c11e872440d11a734658733103ea3a9e
Brian-Enos <brianenos9199@gmail.com>
2023-06-13 13:56:26 +0300

    Adde group 77 to the project for rendering

---
+++ Data-Driven-React/airbnb-app/src/Components/Hero.js
@@ -0,0 +1,14 @@
+
+
+import React from "react" ;
+import ReactDOM from "react-dom";
+import Image from "../Images/Group 77.png";
+
+export default function Hero() {
+  return (
+    <div>
+      <img src={Image} className="hero-image" />
+
+    </div>
+  )
+}
