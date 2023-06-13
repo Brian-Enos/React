f80cb8780232f1964f78960c898c012984f1455e
Brian-Enos <brianenos9199@gmail.com>
2023-06-13 10:48:43 +0300

    Added a classname to the ul items and styled them: remaining color

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -8,7 +8,7 @@ export default function Main() {
   return (
     <main>
       <h1 className="main--title">Fun Facts About React</h1>
-      <ul>
+      <ul className="main--facts">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke </li>
         <li>Has well over 100K stars on Github</li>
