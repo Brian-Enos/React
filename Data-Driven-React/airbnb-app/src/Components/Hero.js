8bcd49d9bc437bb3637c05af97f623f7800fa822
Brian-Enos <brianenos9199@gmail.com>
2023-06-13 14:19:33 +0300

    Assigned classnames to hero JSX and used those classes to style it in css

--- Data-Driven-React/airbnb-app/src/Components/Hero.js
+++ Data-Driven-React/airbnb-app/src/Components/Hero.js
@@ -6,10 +6,10 @@ import Image from "../Images/Group 77.png";
 
 export default function Hero() {
   return (
-    <section>
+    <section className="hero">
       <img src={Image} className="hero-photo" />
       <h1 className="hero--header">Online Experiences</h1>
-      <p className="hero--text">Join unique interactive activities led bt one-of-a-kind  hosts-all without leaving home</p>
+      <p className="hero--text">Join unique interactive activities led by one-of-a-kind  hosts-all without leaving home</p>
 
     </section>
   )
