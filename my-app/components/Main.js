513b05a592a2f1f2feae82ee33f3e19630309d33
Brian-Enos <brianenos9199@gmail.com>
2023-06-13 09:00:40 +0300

    Created required folders for the project

---
+++ my-app/components/Main.js
@@ -0,0 +1,12 @@
+
+
+import React from "react" ;
+import ReactDOM from "react-dom" ;
+
+function Main() {
+  return (
+    <nav>
+      <h1>Main Component Goes here</h1>
+    </nav>
+  )
+}
---
+++ my-app/components/Navbar.js
@@ -0,0 +1,13 @@
+
+
+import React from "react" ;
+import ReactDOM from "react-dom" ;
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>Navbar Componet Goes Here</h1>
+    </div>
+  )
+}
+
