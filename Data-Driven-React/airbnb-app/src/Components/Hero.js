353f92ba52977824dd64f520bce13f0fb6b1c75e
Brian-Enos <brianenos9199@gmail.com>
2023-06-13 14:03:44 +0300

    Completed typography

--- Data-Driven-React/airbnb-app/src/Components/Hero.js
+++ Data-Driven-React/airbnb-app/src/Components/Hero.js
@@ -6,9 +6,11 @@ import Image from "../Images/Group 77.png";
 
 export default function Hero() {
   return (
-    <div>
-      <img src={Image} className="hero-image" />
+    <section>
+      <img src={Image} className="hero-photo" />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">Join unique interactive activities led bt one-of-a-kind  hosts-all without leaving home</p>
 
-    </div>
+    </section>
   )
 }
