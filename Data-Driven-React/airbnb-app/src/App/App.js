c3750e59f71bf7620d3893d3059348485e781c50
Brian-Enos <brianenos9199@gmail.com>
2023-06-13 13:55:28 +0300

    Hero is now being rendered

--- Data-Driven-React/airbnb-app/src/App/App.js
+++ Data-Driven-React/airbnb-app/src/App/App.js
@@ -3,11 +3,13 @@ import React from "react" ;
 import ReactDOm from "react-dom";
 
 import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero" ;
 
 export default function Nabar() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
