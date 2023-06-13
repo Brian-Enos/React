fb740eb121581d853db870f2e6bb96e5ef771785
Brian-Enos <brianenos9199@gmail.com>
2023-06-13 13:31:38 +0300

    App.js now renders components

---
+++ Data-Driven-React/airbnb-app/src/App/App.js
@@ -0,0 +1,14 @@
+
+import React from "react" ;
+import ReactDOm from "react-dom";
+
+import Navbar from "../Components/Navbar";
+
+export default function Nabar() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
