db47de8719f6a2a5723f6242b6de9f89a9bd20aa
Brian-Enos <brianenos9199@gmail.com>
2023-06-13 09:19:38 +0300

    Moved the compoents folder into the src folder

---
+++ my-app/src/components/App.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+import ReactDOM from "react-dom" ;
+
+import Navbar from "./Navbar";
+import Main from "./Main" ;
+
+
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
---
+++ my-app/src/components/Main.js
@@ -0,0 +1,12 @@
+
+
+import React from "react" ;
+import ReactDOM from "react-dom" ;
+
+export default function Main() {
+  return (
+    <nav>
+      <h1>Main Component Goes here</h1>
+    </nav>
+  )
+}
