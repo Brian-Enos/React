7eea0c7302affe1e78dd71e40a9bf74e5295d99a
Brian-Enos <brianenos9199@gmail.com>
2023-06-14 09:29:07 +0300

    Completed the typography for this section

---
+++ Data-Driven-React/airbnb-app/src/Components/Card.js
@@ -0,0 +1,23 @@
+
+
+import React from "react" ;
+import ReactDOM from "react-dom";
+
+import StarIcon from "../Images/Star 1.png";
+import Image from "../Images/image 12.png" ;
+
+export default function() {
+  return (
+    <div className="card">
+      <img src={Image} />
+      <div className="card-stats">
+        <img src={StarIcon}>
+          <span>5.0</span>
+          <span>(6) . </span>
+          <span>USA</span>
+      </div>
+      <p>Life Lessons with kattie Zaferes </p>
+      <p>From $136 / person</p>
+    </div>
+  )
+}
