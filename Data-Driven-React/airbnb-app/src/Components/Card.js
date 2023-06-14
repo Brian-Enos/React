7c79a99c71d0b4a6111f2c2870fe13490bf5009a
Brian-Enos <brianenos9199@gmail.com>
2023-06-14 10:04:13 +0300

    Styled the image and star icons on the stats section

--- Data-Driven-React/airbnb-app/src/Components/Card.js
+++ Data-Driven-React/airbnb-app/src/Components/Card.js
@@ -5,19 +5,20 @@ import ReactDOM from "react-dom";
 
 import StarIcon from "../Images/Star 1.png";
 import Image from "../Images/image 12.png" ;
+import CSS from "../CSS/styles.css";
 
-export default function() {
+export default function Card() {
   return (
     <div className="card">
-      <img src={Image} />
-      <div className="card-stats">
-        <img src={StarIcon} />
+      <img src={Image} className="card--image" />
+      <div className="card--stats">
+        <img src={StarIcon} className="card--star" />
           <span>5.0</span>
-          <span>(6) . </span>
-          <span>USA</span>
+          <span className="gray">(6) . </span>
+          <span className="gray">USA</span>
       </div>
       <p>Life Lessons with kattie Zaferes </p>
-      <p>From $136 / person</p>
+      <p><span className="bold">From $136</span> / person</p>
     </div>
   )
 }
