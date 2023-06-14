6f05d370d808e704401530d5b35c37d7f5d7088b
Brian-Enos <brianenos9199@gmail.com>
2023-06-14 10:35:10 +0300

    Created a standard html boilerplate and rendered h1 element in App component

---
+++ Data-Driven-React/js-inside-jsx/src/index.js
@@ -0,0 +1,15 @@
+
+import React from "react" ;
+import ReactDOM from "react-dom" ;
+
+function App() {
+
+  const firstName = "Brian";
+  const lastName = "Otieno" ;
+    
+  return (
+    <h1>Hello world!</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
