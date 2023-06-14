735b5cf033387e68bdef73113b3712935a7fc7b6
Brian-Enos <brianenos9199@gmail.com>
2023-06-14 11:00:53 +0300

    rendered time of the day

--- Data-Driven-React/js-inside-jsx/src/index.js
+++ Data-Driven-React/js-inside-jsx/src/index.js
@@ -13,13 +13,33 @@ import ReactDOM from "react-dom" ;
 }
  */
 
-function App() {
+/* function App() {
 
   const date = new Date();
     
   return (
     <h1>It's currently about {date.getHours() % 12}!</h1>
-  )
+  ) */
 
-}
+
+
+  function App() {
+    const date = new Date();
+    const hours = (date.getHours());
+    let timeOfDay ;
+
+    if(hours < 12 ) {
+      timeOfDay = "Good Morining" ;
+    }
+    else if(hours > 12 && hours < 17) {
+      timeOfDay = "Good Afternoon!";
+    }
+    else {
+     timeOfDay = "Good Night";
+    }
+
+    return (
+      <h1>Hi, {timeOfDay}</h1>
+    )
+  }
   ReactDOM.render(<App />, document.getElementById("root"));
