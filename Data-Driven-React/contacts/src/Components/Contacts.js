d985588ac1221a3d095e180e0671e27c6c882f7f
Brian-Enos <brianenos9199@gmail.com>
2023-06-14 11:45:17 +0300

    Made changes and assigned the added images a refernce alias

--- Data-Driven-React/contacts/src/Components/Contacts.js
+++ Data-Driven-React/contacts/src/Components/Contacts.js
@@ -1,6 +1,8 @@
 import React from "react" ;
 import ReactDOM from "react-dom";
 import FirstContact from "../Images/contact-1.png";
+import MailIcon from "../Images/mail-icon.png";
+import PhoneIcon from "../Images/phone-icon.png";
 
 
 export default function Contacts() {
@@ -10,8 +12,21 @@ export default function Contacts() {
        {/*  //first contact begins here */}
 
         <img src={FirstContact} />
+
         <h3>Mr. Whiskerson</h3>
+
+        <div className="info-group">
+          <img src={PhoneIcon} />
+          <p>(212) 555-1234</p>
+        </div>
+
+        <div className="info-group">
+          <img src={MailIcon} />
+          <p>mr.whiskerz@catnap.meow</p>
+        </div>
+
       </div>
+
     </div>
   )
 }
