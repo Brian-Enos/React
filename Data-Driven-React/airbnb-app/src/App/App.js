dc09d034ebb8f14f9a2295e4e83dfa1c3076efeb
Brian-Enos <brianenos9199@gmail.com>
2023-06-14 09:33:20 +0300

    rendering the card.js file

--- Data-Driven-React/airbnb-app/src/App/App.js
+++ Data-Driven-React/airbnb-app/src/App/App.js
@@ -4,12 +4,14 @@ import ReactDOm from "react-dom";
 
 import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero" ;
+import Card from "../Components/Card";
 
 export default function Nabar() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   )
 }
