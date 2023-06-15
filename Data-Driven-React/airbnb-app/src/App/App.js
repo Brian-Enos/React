525ef6b0faca7b0420438a19c9222f5a508e182d
Brian-Enos <brianenos9199@gmail.com>
2023-06-15 11:45:59 +0300

    Passed props to the Card element

--- Data-Driven-React/airbnb-app/src/App/App.js
+++ Data-Driven-React/airbnb-app/src/App/App.js
@@ -6,12 +6,24 @@ import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero" ;
 import Card from "../Components/Card";
 
+import Icon from "../Images/Star 1-1.png";
+import Image from "../Images/image 12.png";
+
 export default function Nabar() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img={Image}
+        icon={Icon}
+        rating="6"
+        reviewCount="(6)"
+        country="Nairobi"
+        title="Life Lessons with Kattie Zaferas"
+        price="136"
+
+       />
     </div>
   )
 }
