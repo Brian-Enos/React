5f729c016a0e3a47e21b0d1ecaa35bb9913e086b
Brian-Enos <brianenos9199@gmail.com>
2023-06-15 10:43:06 +0300

    Craeted all the necessary files for the project

---
+++ Data-Driven-React/challenge/src/App/App.js
@@ -0,0 +1,18 @@
+import React from "react" ;
+import ReactDOM from "react-dom" ;
+import Joke from "../Components/Joke";
+
+function App() {
+  return (
+    <>
+    <Joke />
+
+    <Joke />
+
+    <Joke />
+
+    </>
+  )
+}
+
+export default App ;
