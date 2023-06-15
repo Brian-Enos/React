c602677df1c1ed5212b37a86fd6b7f05e96297a8
Brian-Enos <brianenos9199@gmail.com>
2023-06-15 12:50:51 +0300

    Generted a data file to e usde for the project and all the necessary content

---
+++ Data-Driven-React/airbnb-app/src/Components/Data.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    id: 1,
+    title: "Life lessons with Katties Zaferas" ,
+    description: "I will share with you what I call \"Positive Impactful moments of Disappointment\". Throughout my carrer, many of the highest moments only come after disappointments...." ,
+    price: 136 ,
+    coverImg: "../Images/image 12.png",
+    stats: {
+      rating:5 ,
+      reviewCount: 6
+    },
+    location: "online",
+    openSports: 0,
+  },
+
+
+
+  {
+    id: 2,
+    title: "Learn wedding photography", 
+    description: "Interested in becoming a wedding photographer? For beginners and experienced photographers alike, join us in learning techniques ",
+    price: 125,
+    coverImg: "wedding-photography.png" ,
+    stats: {
+      rating:5 ,
+      reviewCount: 30
+    },
+    location: "online",
+    openSports: 27,
+
+
+  },
+
+
+
+  {
+
+    id: 3, 
+    title: "Group Mountain Biking",
+    description: "experience the beatiful norweighian landscape and meet new friends all while conquering rugged terrain on your mountain bike" ,
+    price: 50, 
+    coverImg: "mountain-bike.png",
+    stats: {
+      rating:4.8 ,
+      reviewCount: 2
+    },
+    location: "Norway",
+    openSports: 3,
+
+  }
+
+
+
+]
